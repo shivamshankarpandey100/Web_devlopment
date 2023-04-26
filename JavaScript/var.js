4335b5b9d8e20c794dabfc2c2010c93729c86dbc
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-04-26 20:11:45 +0530

    26-04-2023

--- JavaScript/var.js
+++ JavaScript/var.js
@@ -14,3 +14,10 @@ var a=10;
 console.log(typeof a);
 
 console.log(a);
+//var has some problem 
+//we can use to change line in java script then we use "\n"
+var b="hi i hope  you gyse are able to grasp the concept";
+console.log(b);
+
+var b='hi i hope you gyse are able to grasp the concept';
+console.log(b);
