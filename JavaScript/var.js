a39647beafa9996d09e1b486606a948ac7be99b8
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-04-26 20:03:06 +0530

    26-04-2023

---
+++ JavaScript/var.js
@@ -0,0 +1,16 @@
+var a=10;
+console.log(typeof a);
+
+var a =10.567;
+console.log(typeof a);
+
+var a="10";
+console.log(typeof a);
+
+var a=true;
+console.log(typeof a);
+
+var a=10;
+console.log(typeof a);
+
+console.log(a);
