09dbbd42273b64f616d7bb83d99f6522f0481013
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-04-26 19:51:13 +0530

    26-04-2023

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+//starting js javaScript
+// var,let,const
+//variable declearation
+var a;//int String float?
+a=10;
+console.log(a);//for output
+a="hello World";
+console.log(a);// javaScript print hello world
+//Typeof
+console.log(typeof a);
+
+//boolean
+a=true;
+console.log(typeof a);
+
+//objective
+a=null;
+console.log(typeof a);
