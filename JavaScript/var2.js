d3db81b4328105584a290fa0732383ceae474fbd
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-04-26 20:31:07 +0530

    26-04-2023

---
+++ JavaScript/var2.js
@@ -0,0 +1,13 @@
+//var has some problem
+var r="hello world";
+console.log(r);
+//There r variable is re_declearation
+var r=100;
+console.log(r);
+
+//then the resolvemthe problem of var we can use let 
+let l="shivam";
+console.log(l);
+
+// let l="shivam";
+// console.log(l);
