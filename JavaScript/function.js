800489b8dbc2e15b962405cd3b46c8532dab9188
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-05-01 17:08:17 +0530

    01-05-2023

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,1 +1,23 @@
-//this the function file
+// //this the function file
+// //in javaScript ther is three type of function
+
+// function add(a,b){
+//     return a+b;
+
+// }
+// let b=add(4,6);
+// console.log(b);
+function calculator(str,a,b){
+if(str=="add"){
+    return function add(){
+        console.log (a+b);
+    }
+}
+else if(str=="sub"){
+    return function sub(){
+        console.log (a-b);    }
+}
+
+}
+let i=calculator("sub",5,3);
+i();
