c608c1144013cadcbf28dfb6c4e8eab3bf291c0a
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-05-01 17:39:14 +0530

    01-05-2023

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -22,11 +22,24 @@ else if(str=="sub"){
 let i=calculator("sub",5,3);
 i();
 // for invoking the function and call the function which stored in the variable use "()" 
-let sayHi=function(){
+var sayHi=function(){
     console.log("hello gyse iam the function Exprssion");
 }
 
 sayHi();
+//revoke function means jab variable me function aa jaye to uske aage small bracket laga kar usko run kar dete hai jis se function chal jata hai
+
+
+// home work sub add multiplication div
+
+
+//third typr of function is IIFE 
+let r= (function add(a,b){
+    return a+b;
+})(5,4);
+//console.log(r(5,4));
+console.log(r);
+
 
 
 
