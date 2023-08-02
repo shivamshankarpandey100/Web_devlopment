8c9e8fef0b41f62885f7c1193caff9e5c2d8f8a2
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-08-02 10:10:36 +0530

    string concatignation is done

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -15,3 +15,24 @@ console.log(str.slice(6));
 console.log(str.slice(0));
 console.log(str.slice(6,11));
 console.log(str.slice(0,-4));
+//subString method
+let ans=str.substr(2,11);
+console.log(ans);
+console.log(str);
+//replacing
+
+//to lower case
+let str1="YOLO";
+
+
+console.log(str1.toLowerCase());
+//to Upper case
+var str2="lowo";
+console.log(str2.toUpperCase());
+
+
+//concatination method 
+let firstStr="hey ";
+let secondStr="how are you";
+let concatenatedStr=firstStr+secondStr;
+console.log(concatenatedStr);
