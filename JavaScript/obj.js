d4cad65b3aaca52fb36589c94af8a99a5566602c
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-08-03 09:42:10 +0530

    ${obj.key} to get value

---
+++ JavaScript/obj.js
@@ -0,0 +1,16 @@
+let obj={};
+console.log(obj);
+console.log(typeof obj);
+let person={
+    name:"Shivam",
+    age:22,
+    phone:1234567890,
+    gender:"male",
+    height:"172cm"
+}
+console.log(person);
+console.log(person.name);
+console.log(person.gender);
+console.log(person.height);
+
+console.log(`hello usre your name is ${person.name} and you are a ${person.gender} and you height is ${person.height}`);
