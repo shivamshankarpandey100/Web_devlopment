799a66065beef559787acb969d3d66d7b981725d
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-08-03 09:59:27 +0530

    objects started

--- JavaScript/obj.js
+++ JavaScript/obj.js
@@ -11,6 +11,30 @@ let person={
 console.log(person);
 console.log(person.name);
 console.log(person.gender);
+console.log(person["gender"]);//it is a way to use key
 console.log(person.height);
 
 console.log(`hello usre your name is ${person.name} and you are a ${person.gender} and you height is ${person.height}`);
+//nestin of object
+let captionAmerica={
+    firstName:"steve",
+    lastname:"rogers",
+    friends:["Bucky","Tony Stark","brue banner"],
+    address:{
+        state:"manhaten",
+        city:"New York",
+        country:"USA"
+    },
+    sayHi:function(){
+        console.log(`hello my name is ${this.firstName}`);
+    }
+};
+console.log(captionAmerica);
+console.log(captionAmerica.friends)
+console.log(captionAmerica.sayHi());//method accessing
+
+//method besicaly a function in the object;
+
+
+
+
