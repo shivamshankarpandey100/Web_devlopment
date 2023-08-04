d46f1821ec54cc38dc29b599d4d2c25d9482bafc
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-08-04 11:31:02 +0530

    how to work js

--- JavaScript/obj.js
+++ JavaScript/obj.js
@@ -43,15 +43,3 @@ for(let key in captionAmerica){
     console.log(captionAmerica[key]);
 }
 
-var num=5;
-function cube(num){
-    var res=num*num*num;
-    return res;
-}
-var ans1=cube(num);
-var ans2=cube(10);
-var ans3=cube();
-console.log(ans1);
-
-console.log(ans2);
-console.log(ans3);
