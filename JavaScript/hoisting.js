56a69422a81bc7e43107995805f4abc2f92a03c1
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-15 15:02:39 +0530

    3.0

--- JavaScript/hoisting.js
+++ JavaScript/hoisting.js
@@ -6,3 +6,14 @@ function greet(){
     console.log("hello hero");
 
 }
+console.log(a);
+ngreet();
+// hello();
+var a=10;
+function ngreet(){
+    console.log("helllo friends");
+}
+var hello=function (){
+    console.log("you gyse rock");
+}
+hello();
