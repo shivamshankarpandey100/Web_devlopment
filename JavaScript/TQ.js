2b9dba7a032344eabeab0346f5f7a76676fee967
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-22 10:07:01 +0530

    that is String

---
+++ JavaScript/TQ.js
@@ -0,0 +1,8 @@
+let randomValue={name:"layla"};
+randomValue=23;
+if(!typeof randomValue === "String"){
+    console.log("it's not a String");
+}else{
+    console.log("yay it's a String");
+}
+//type of operater returns value in String//"number"
