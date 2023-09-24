8506e1bee2f33271ed68de950a47b079d1c84340
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-24 22:23:18 +0530

    tq.js

--- JavaScript/TQ.js
+++ JavaScript/TQ.js
@@ -3,6 +3,29 @@ randomValue=23;
 if(!typeof randomValue === "String"){
     console.log("it's not a String");
 }else{
-    console.log("yay it's a String");
+    console.log("yay it's a String"+randomValue);
 }
+
+// ans= yay it's a String23
 //type of operater returns value in String//"number"
+console.log("-------------------------------------------------------------");
+
+//this iste user name object and the this keyword use to access or target inner key using this like 
+//this.email
+const user={
+    email:"skndsk@gamil.com",
+    updateEmail:function(email){
+        this.email=email;
+    }
+}
+user.updateEmail("new@email");
+console.log(user.email);
+
+//ans = new@email
+
+console.log("-------------------------------------------------------------");
+const fruit=["🍌","🍊","🍎"]; //'🍇']
+fruit.slice(0,1);
+fruit.splice(0,1);
+fruit.unshift("🍇");
+console.log(fruit);
