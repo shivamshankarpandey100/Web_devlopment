98624ffed937f5fe89d21f1a88eae01f50479275
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-29 11:06:09 +0530

    lvc

---
+++ JavaScript/lvc.js
@@ -0,0 +1,10 @@
+//let var and const declearation and diffrence
+console.log(x);
+var x=10;
+let b=100;
+console.log(b);
+//hoisting is the method jisme sare variable ko memory alocate ho jati bina ek bhi line execute kiye bina 
+
+
+
+
