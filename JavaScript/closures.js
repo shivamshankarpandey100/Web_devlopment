7068eff64861c2b5ebab3b84f6c8c2d5c99c89fe
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-30 13:45:32 +0530

    closures

--- JavaScript/closures.js
+++ JavaScript/closures.js
@@ -7,18 +7,30 @@
 //     inner();
 // }
 // outer();
-function outer(){
-    var a=10;
-    function inner(){
-        console.log(a);
-    }
-    return inner;
-}
-var z=outer();
-console.log(z+"");
+// function outer(){
+//     var a=10;
+//     function inner(){
+//         console.log(a);
+//     }
+//     return inner;
+// }
+// var z=outer();
+// console.log(z+"");
 
 //inner return hote hi function return ho gya 
 // function inner(){
 //     console.log(a);
 // }
-z();//it is gives the 10 because the z is full function of inner
+// z();//it is gives the 10 because the z is full function of inner
+function outer(){
+    var a=7;
+    function inner(){
+        console.log(a);
+    }
+    a=10;
+    return inner;
+
+}
+var c=outer();
+console.log(c);
+c();
