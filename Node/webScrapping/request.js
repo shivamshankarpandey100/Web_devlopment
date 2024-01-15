38a6b7983651bed10c909298ab234b14319c7f0a
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-01-15 21:49:01 +0530

    web_scrapping

--- Node/webScrapping/request.js
+++
@@ -1,7 +0,0 @@
-const request = require('request');
-request("https://www.worldometers.info/coronavirus/",cb);
-function cb(err, res, body){
-    console.error("error",err);//give the error
-    console.log(res);//give the responce
-    console.log(body);//print body
-}
