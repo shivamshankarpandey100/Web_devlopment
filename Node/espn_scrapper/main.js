4462021c8e380159d8a79361be848540ec96997b
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-01-18 19:06:20 +0530

    web site Structure is diffrent

--- Node/espn_scrapper/main.js
+++ Node/espn_scrapper/main.js
@@ -17,9 +17,13 @@ function cb(err, res, html){
 function handle(html){
     let selectertool=cheerio.load(html);
     let spanelement=selectertool('span[class="ds-text-tight-m ds-font-bold ds-text-typo-primary hover:ds-underline hover:ds-decoration-ui-stroke-primary ds-block ds-block ds-text-center"]');
-    console.log(spanelement.html());
+    console.log(spanelement);
+    let relativelink=spanelement.attr("href");
+    console.log(relativelink);
+
+    let fulllink="https://www.espncricinfo.com";
 }
 
-
-
+// currentPage link=https://www.espncricinfo.com/series/ipl-2020-21-1210595
+//  nextPage link=https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-schedule-fixtures-and-results
 //node main.js
