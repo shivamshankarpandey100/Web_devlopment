fa11757e1c49d1f8e060e9d57bc0acdd28b4d880
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-03-04 20:06:09 +0530

    :rocket: setTimeout :rocket:

--- Node/Automation/setTimeout.js
+++ Node/Automation/setTimeout.js
@@ -1,4 +1,4 @@
-function yolo(){
+{function yolo(){
     var a=10;
    function cb(){
     console.log("how Are you");
@@ -10,3 +10,29 @@ console.log(a);
 yolo();
 let b=100;
 console.log(b);
+}
+
+
+
+const request=require("request");
+
+let url="https://www.youtube.com";
+function yolo(){
+    var a=10;
+   function cb(){
+    if(err){
+        console.log(err);
+    
+    }
+    else{
+        console.log("how Are you");
+    }
+    
+}
+request(url,cb);
+console.log(a);
+}
+
+yolo();
+let x=100;
+console.log(x);
