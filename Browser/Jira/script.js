0a2b7e2b37d30981be651afcbca6fc8d8d3846c3
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-03-26 21:06:43 +0530

    :lock::zap: Ticket color and data fetching the Page :zap::lock:

--- Browser/Jira/script.js
+++ Browser/Jira/script.js
@@ -1,5 +1,9 @@
 const addBtn=document.querySelector(".add-btn");
 const modalCont=document.querySelector(".modal-cont");
+let colors=["lightpink","lightblue","lightgreen","black"];
+let modalPriorityColor=colors[colors.length-1];//black
+const allPriorityColors = document.querySelectorAll(".priority-color");
+let textAreaCont = document.querySelector(".textarea-cont");
 
 let isModalpresent=false;
 addBtn.addEventListener('click',function(){
@@ -14,12 +18,30 @@ addBtn.addEventListener('click',function(){
     isModalpresent=!isModalpresent;
 })
 
-const allPriorityColors=document.querySelectorAll(".priority-color");
+
 allPriorityColors.forEach(function(colorElement){
     colorElement.addEventListener('click',function(){
         allPriorityColors.forEach(function(priorityColorElem){
             priorityColorElem.classList.remove("active");
         });
         colorElement.classList.add("active");
+        modalPriorityColor=colorElement.classList[0];
     });
 });
+
+
+modalCont.addEventListener("keydown",function(e){
+    let key=e.key;
+    if(key=="Shift"){
+        console.log(modalPriorityColor);
+        console.log(textAreaCont.value);
+        createTicket(modalPriorityColor,textAreaCont.value);
+
+        modalCont.contains.style.display="none";
+        isModalpresent=false;
+    }
+});
+
+function createTicket(ticketColor, data){
+    
+}
